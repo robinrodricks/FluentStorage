00a90354550637ad066ef9b3a66484f63fc63335
Ivan Gavryliuk <ivan.gavrilyuk@gmail.com>
2016-01-12 23:21:11 +0000

    saving work on S3

---
+++ src/Storage.Net.Net45.Aws/Blob/AwsS3BlobStorage.cs
@@ -0,0 +1,48 @@
+﻿using Storage.Net.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Storage.Net.Aws.Blob
+{
+   class AwsS3BlobStorage : IBlobStorage
+   {
+      public AwsS3BlobStorage()
+      {
+         //IAmazonS3 client;
+      }
+
+      public void Delete(string id)
+      {
+         throw new NotImplementedException();
+      }
+
+      public void DownloadToStream(string id, Stream targetStream)
+      {
+         throw new NotImplementedException();
+      }
+
+      public bool Exists(string id)
+      {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<string> List(string prefix)
+      {
+         throw new NotImplementedException();
+      }
+
+      public Stream OpenStreamToRead(string id)
+      {
+         throw new NotImplementedException();
+      }
+
+      public void UploadFromStream(string id, Stream sourceStream)
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
