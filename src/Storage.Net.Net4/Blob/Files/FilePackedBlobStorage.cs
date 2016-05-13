11843a2922e5b97e56949dafa176bcd2dfaf3a99
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2016-05-13 11:12:48 +0100

    fixing platform multitarget

---
+++ src/Storage.Net.Net4/Blob/Files/FilePackedBlobStorage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Net.Blob.Files
+{
+   /// <summary>
+   /// File based blob storage packed into one single file.
+   /// </summary>
+   public class FilePackedBlobStorage : IBlobStorage
+   {
+      public FilePackedBlobStorage(Stream fileStream)
+      {
+         
+      }
+
+      public IEnumerable<string> List(string prefix)
+      {
+         throw new NotImplementedException();
+      }
+
+      public void Delete(string id)
+      {
+         throw new NotImplementedException();
+      }
+
+      public void UploadFromStream(string id, Stream sourceStream)
+      {
+         throw new NotImplementedException();
+      }
+
+      public void DownloadToStream(string id, Stream targetStream)
+      {
+         throw new NotImplementedException();
+      }
+
+      public Stream OpenStreamToRead(string id)
+      {
+         throw new NotImplementedException();
+      }
+
+      public bool Exists(string id)
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
