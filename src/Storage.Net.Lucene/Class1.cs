5569f382cf9bf0672fa4ae9c8c018fa919f633b2
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2016-07-19 16:42:59 +0100

    lucene stub

---
+++ src/Storage.Net.Lucene/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage.Net.Lucene
+{
+    public class Class1
+    {
+    }
+}
