42e99257fb8b39dcac9b66e8a0933ee697f3b506
ivang <ivang@DESKTOP-1QTFOLB>
2017-01-24 11:13:50 +0000

    a working script to update project versions

--- ChangeVersion.ps1
+++ ChangeVersion.ps1
@@ -7,7 +7,7 @@ Write-Host "version is $Version"
 
 function Update-ProjectJsonVersions([string]$RelPath, [string]$Version, [bool]$UpdatePackageVersion)
 {
-   $path = "$PSScriptRoot\$RelPath"
+   $path = "$PSScriptRoot\$RelPath\project.json"
    Write-Host "processing $path"
    $json = Get-Content $path | ConvertFrom-Json
 
@@ -20,7 +20,7 @@ function Update-ProjectJsonVersions([string]$RelPath, [string]$Version, [bool]$U
    
    foreach($other in $args)
    {
-      $json.$other = $Version
+      $json.dependencies.$other = $Version
 
       Write-Host "set $other to $Version"
    }
@@ -33,23 +33,8 @@ function Update-ProjectJsonVersions([string]$RelPath, [string]$Version, [bool]$U
    $content | Set-Content -Path $path
 }
 
-Update-ProjectJsonVersions "src\src\Storage.Net\project.json" $Version $true
-
-<#
-$jsonMain = Get-Json "src\Config.Net\project.json"
-$jsonAzure = Get-Json "src\Config.Net.Azure\project.json"
-$jsonTests = Get-Json "src\Config.Net.Tests\project.json"
-
-$jsonMain.version = $Version
-
-$jsonAzure.version = $Version
-$jsonAzure.dependencies."Config.Net" = $Version
-
-$jsonTests.dependencies."Config.Net" = $Version
-$jsonTests.dependencies."Config.Net.Azure" = $Version
-
-Set-Json $jsonMain "src\Config.Net\project.json"
-Set-Json $jsonAzure "src\Config.Net.Azure\project.json"
-Set-Json $jsonTests "src\Config.Net.Tests\project.json"
-
-#>
+Update-ProjectJsonVersions "src\src\Storage.Net" $Version $true
+Update-ProjectJsonVersions "src\src\Storage.Net.Tests" $Version $false "Storage.Net" "Storage.Net.Amazon.Aws" "Storage.Net.Microsoft.Azure" "Storage.Net.Esent"
+Update-ProjectJsonVersions "src\src\Storage.Net.Amazon.Aws" $Version $true "Storage.Net"
+Update-ProjectJsonVersions "src\src\Storage.Net.Esent" $Version $true "Storage.Net"
+Update-ProjectJsonVersions "src\src\Storage.Net.Microsoft.Azure" $Version $true "Storage.Net"
