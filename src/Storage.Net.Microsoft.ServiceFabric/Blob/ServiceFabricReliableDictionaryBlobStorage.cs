7f9f76660121cd1ffaf73749a027de256f86638d
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-08-30 11:50:20 +0100

    asf compiles

--- src/Storage.Net.Microsoft.ServiceFabric/Blob/ServiceFabricReliableDictionaryBlobStorage.cs
+++ src/Storage.Net.Microsoft.ServiceFabric/Blob/ServiceFabricReliableDictionaryBlobStorage.cs
@@ -20,60 +20,7 @@ namespace Storage.Net.Microsoft.ServiceFabric.Blob
          _collectionName = collectionName ?? throw new ArgumentNullException(nameof(collectionName));
       }
 
-      public override async Task AppendAsync(string id, Stream sourceStream)
-      {
-         using (var tx = await OpenCollection())
-         {
-            //create a new byte array with
-            byte[] extra = sourceStream.ToByteArray();
-            ConditionalValue<byte[]> value = await tx.Collection.TryGetValueAsync(tx.Tx, id);
-            int oldLength = value.HasValue ? value.Value.Length : 0;
-            byte[] newData = new byte[oldLength + extra.Length];
-            if(value.HasValue)
-            {
-               Array.Copy(value.Value, newData, oldLength);
-            }
-            Array.Copy(extra, 0, newData, oldLength, extra.Length);
-
-            //put new array into the key
-            await tx.Collection.AddOrUpdateAsync(tx.Tx, id, extra, (k, v) => extra);
-
-            //commit the transaction
-            await tx.CommitAsync();
-         }
-      }
-
-      public override async Task DeleteAsync(string id)
-      {
-         using (var tx = await OpenCollection())
-         {
-            await tx.Collection.TryRemoveAsync(tx.Tx, id);
-
-            await tx.CommitAsync();
-         }
-      }
-
-      public override async Task<bool> ExistsAsync(string id)
-      {
-         using (var tx = await OpenCollection())
-         {
-            return await tx.Collection.ContainsKeyAsync(tx.Tx, id);
-         }
-      }
-
-      public override async Task<BlobMeta> GetMetaAsync(string id)
-      {
-         using (var tx = await OpenCollection())
-         {
-            ConditionalValue<byte[]> value = await tx.Collection.TryGetValueAsync(tx.Tx, id);
-
-            if (!value.HasValue) return null;
-
-            return new BlobMeta(value.Value.Length, null);
-         }
-      }
-
-      protected override async Task<IEnumerable<BlobId>> ListAsync(string[] folderPath, string prefix, bool recurse, CancellationToken cancellationToken)
+      public async Task<IEnumerable<BlobId>> ListAsync(string folderPath, string prefix, bool recurse, CancellationToken cancellationToken)
       {
          var result = new List<BlobId>();
 
@@ -99,19 +46,19 @@ namespace Storage.Net.Microsoft.ServiceFabric.Blob
          return result;
       }
 
-      public override async Task<Stream> OpenReadAsync(string id)
+      public async Task WriteAsync(string id, Stream sourceStream, bool append)
       {
-         using (var tx = await OpenCollection())
+         if (append)
          {
-            ConditionalValue<byte[]> value = await tx.Collection.TryGetValueAsync(tx.Tx, id);
-
-            if (!value.HasValue) throw new StorageException(ErrorCode.NotFound, null);
-
-            return new MemoryStream(value.Value);
+            await AppendAsync(id, sourceStream);
+         }
+         else
+         {
+            await WriteAsync(id, sourceStream);
          }
       }
 
-      public override async Task WriteAsync(string id, Stream sourceStream)
+      private async Task WriteAsync(string id, Stream sourceStream)
       {
          byte[] value = sourceStream.ToByteArray();
 
@@ -123,11 +70,104 @@ namespace Storage.Net.Microsoft.ServiceFabric.Blob
          }
       }
 
+      private async Task AppendAsync(string id, Stream sourceStream)
+      {
+         using (var tx = await OpenCollection())
+         {
+            //create a new byte array with
+            byte[] extra = sourceStream.ToByteArray();
+            ConditionalValue<byte[]> value = await tx.Collection.TryGetValueAsync(tx.Tx, id);
+            int oldLength = value.HasValue ? value.Value.Length : 0;
+            byte[] newData = new byte[oldLength + extra.Length];
+            if(value.HasValue)
+            {
+               Array.Copy(value.Value, newData, oldLength);
+            }
+            Array.Copy(extra, 0, newData, oldLength, extra.Length);
+
+            //put new array into the key
+            await tx.Collection.AddOrUpdateAsync(tx.Tx, id, extra, (k, v) => extra);
+
+            //commit the transaction
+            await tx.CommitAsync();
+         }
+      }
+
+      public async Task<Stream> OpenReadAsync(string id)
+      {
+         using (var tx = await OpenCollection())
+         {
+            ConditionalValue<byte[]> value = await tx.Collection.TryGetValueAsync(tx.Tx, id);
+
+            if (!value.HasValue) throw new StorageException(ErrorCode.NotFound, null);
+
+            return new MemoryStream(value.Value);
+         }
+      }
+
+      public async Task DeleteAsync(IEnumerable<string> ids)
+      {
+         using (var tx = await OpenCollection())
+         {
+            foreach (string id in ids)
+            {
+               await tx.Collection.TryRemoveAsync(tx.Tx, id);
+            }
+
+            await tx.CommitAsync();
+         }
+      }
+
+      public async Task<IEnumerable<bool>> ExistsAsync(IEnumerable<string> ids)
+      {
+         var result = new List<bool>();
+         using (var tx = await OpenCollection())
+         {
+            foreach (string id in ids)
+            {
+               bool exists = await tx.Collection.ContainsKeyAsync(tx.Tx, id);
+
+               result.Add(exists);
+            }
+         }
+         return result;
+      }
+
+      public async Task<IEnumerable<BlobMeta>> GetMetaAsync(IEnumerable<string> ids)
+      {
+         GenericValidation.CheckBlobId(ids);
+
+         var result = new List<BlobMeta>();
+
+         using (var tx = await OpenCollection())
+         {
+            foreach (string id in ids)
+            {
+               ConditionalValue<byte[]> value = await tx.Collection.TryGetValueAsync(tx.Tx, id);
+
+               if (!value.HasValue)
+               {
+                  result.Add(null);
+               }
+               else
+               {
+                  var meta = new BlobMeta(value.Value.Length, null);
+                  result.Add(meta);
+               }
+            }
+         }
+         return result;
+      }
+
       private async Task<FabricTransactionManager<IReliableDictionary<string, byte[]>>> OpenCollection()
       {
          var collection = await _stateManager.GetOrAddAsync<IReliableDictionary<string, byte[]>>(_collectionName);
 
          return new FabricTransactionManager<IReliableDictionary<string, byte[]>>(_stateManager, collection);
+      }
+
+      public void Dispose()
+      {
       }
    }
 }
