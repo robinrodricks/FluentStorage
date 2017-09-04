b06f3d0c33a3b886c01021488c2b73ff69e9206c
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-09-04 14:14:03 +0100

    down to 7 failing tests

--- src/Storage.Net/Blob/ListOptions.cs
+++ src/Storage.Net/Blob/ListOptions.cs
@@ -1,11 +1,30 @@
 ﻿namespace Storage.Net.Blob
 {
+   /// <summary>
+   /// Options for listing storage content
+   /// </summary>
    public class ListOptions
    {
+      private string _prefix;
+
       public string FolderPath { get; set; }
 
-      public string Prefix { get; set; }
+      /// <summary>
+      /// Prefix to filter the name by
+      /// </summary>
+      public string Prefix
+      {
+         get => _prefix;
+         set
+         {
+            GenericValidation.CheckBlobPrefix(value);
+            _prefix = Prefix;
+         }
+      }
 
+      /// <summary>
+      /// When true, operation will recursively navigate down the folders
+      /// </summary>
       public bool Recurse { get; set; }
    }
 }
