e29066e7727fefa3d95383b7992e7e5e98082cf4
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-11-10 15:08:55 +0000

    test strict versions

--- appveyor.ps1
+++ appveyor.ps1
@@ -11,11 +11,6 @@ if($bn -eq $null)
 }
 
 $vt = @{
-   "Storage.Net.Microsoft.Azure.DataLake.Store.csproj" = "3.0.$bn";
-   "Storage.Net.Microsoft.Azure.EventHub.csproj" = "3.0.$bn";
-   "Storage.Net.Microsoft.Azure.ServiceBus.csproj" = "3.0.$bn";
-   "Storage.Net.Microsoft.Azure.Storage.csproj" = "3.0.$bn";
-   "Storage.Net.Microsoft.Azure.KeyVault.csproj" = "2.0.$bn";
    "Storage.Net.Microsoft.ServiceFabric.csproj" = "2.6.204.$bn";
    "Storage.Net.WebHDFS" = "1.0.alpha-$bn"
 }
