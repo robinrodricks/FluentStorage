ea3baafdf1523dc29ee51c4d9d5869b94be014e2
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2018-05-23 10:57:55 +0100

    IMessagePublisher extension method

---
+++ src/Storage.Net/Messaging/MessagePublisherExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace Storage.Net.Messaging
+{
+   /// <summary>
+   /// Extensions for IMessagePublisher
+   /// </summary>
+   public static class MessagePublisherExtensions
+   {
+      /// <summary>
+      /// Puts a new message to the back of the queue.
+      /// </summary>
+      public static Task PutMessageAsync(this IMessagePublisher messagePublisher, QueueMessage message)
+      {
+         if (message == null) return Task.FromResult(false);
+
+         return messagePublisher.PutMessagesAsync(new[] { message });
+      }
+   }
+}
