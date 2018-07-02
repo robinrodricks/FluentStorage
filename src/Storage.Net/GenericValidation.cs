c90250a7a1d1b796a47618bebd6ebca17e48aad3
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2018-07-02 15:39:51 +0100

    upgrading ADLA dependencies

--- src/Storage.Net/GenericValidation.cs
+++ src/Storage.Net/GenericValidation.cs
@@ -66,7 +66,7 @@ namespace Storage.Net
 
          try
          {
-            var l = inputStream.Length;
+            long l = inputStream.Length;
          }
          catch(NotSupportedException ex)
          {
