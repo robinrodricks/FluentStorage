10cef1cff7684e3e8e7425848f0f73d61f89ca4d
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2018-08-10 12:12:40 +0100

    remove failing unit test as it requires network access

--- src/Storage.Net.Tests/StorageInstantiationTest.cs
+++ src/Storage.Net.Tests/StorageInstantiationTest.cs
@@ -29,13 +29,5 @@ namespace Storage.Net.Tests
 
          Assert.Equal(TestDir.FullName, ddbs.RootDirectory.FullName);
       }
-
-      [Fact]
-      public void External_module_storage_created_with_connection_string()
-      {
-         IBlobStorage storage = StorageFactory.Blobs.FromConnectionString("azure.blob://account=test;container=rrr;key=0RlOLDEu8Zn0HmfQ3hnODdAUqEwLOOKq0njK/TaKuYM4LTpyBDozq2LgmD/O9i/0yVP3S/7+sKNYJqmN9+ME9A==;createIfNotExists=false");
-
-         Assert.IsType<AzureUniversalBlobStorageProvider>(storage);
-      }
    }
 }
