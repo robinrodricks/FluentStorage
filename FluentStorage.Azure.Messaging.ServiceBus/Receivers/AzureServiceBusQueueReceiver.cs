f4cf448323a159e6137fdb07e75f539014de1971
Evolutionlab <info@evolutionlab.it>
2023-10-27 21:34:44 +0200

    New AzureServiceBus plugin with the supported package and utility methods to easy manage queues, topics and subscriptions

---
+++ FluentStorage.Azure.Messaging.ServiceBus/Receivers/AzureServiceBusQueueReceiver.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace FluentStorage.Azure.Messaging.ServiceBus.Receivers {
+	/// <summary>
+	/// Implements message receiver on Azure Service Bus Queues
+	/// </summary>
+	internal class AzureServiceBusQueueReceiver : AzureServiceBusReceiver {
+		internal AzureServiceBusQueueReceiver(string connectionString, string queueName,
+		                                       ServiceBusClientOptions clientOptions = null,
+		                                       ServiceBusProcessorOptions processorOptions = null)
+			: base(connectionString, queueName, "", "", clientOptions, processorOptions) {
+		}
+	}
+}
